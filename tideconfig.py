6248a496eee9eee7d69d9dc7fa134fb65076c768
Colby <colby.powers27@gmail.com>
2021-02-07 15:10:23 -0600

    Update tideconfig.py

--- tideconfig.py
+++ tideconfig.py
@@ -1,16 +1,17 @@
 #!/usr/bin/env python
 
+# DO NOT UPLOAD THIS FILE WITH YOUR CREDENTIALS IN IT! TURN ON 2 FACTOR AUTHENTICATION JUST IN CASE!
 rh = {
-    'username': '',
-    'password': '',
+    'username': 'yourUserNameHere',
+    'password': 'yourPasswordHere',
 }
 
+# Modify these values to fit your trading strategy.
 config = {
-    'sellLimit': 0.01, # % profit required to seel (default 1%)
-    'movingAverageWindows': 20,
-    'runMinute': [0, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30, 32, 34, 36, 38, 40, 42, 44, 46, 48, 50, 52, 54, 56, 58,], # 0, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55
-    'minutesBetweenUpdates': 2,
+    'sellLimit': 0.01, # Percentage profit required to sell. 
+    'movingAverageWindows': 20, # How many periods to consider for MA.
+    'runMinute': [0, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30, 32, 34, 36, 38, 40, 42, 44, 46, 48, 50, 52, 54, 56, 58,], # What minute, on the minute, data will bre refreshed. (this is also when buy/sell conditions are checked and executed) 
     'coinList': ['BTC', 'LTC', 'ETH',],
-    'tradesEnabled': False,
-    'rsiWindow': 10,
+    'tradesEnabled': False, # When set to True, real buys/sells are possible.
+    'rsiWindow': 10, # How many periods to consider for RSI.
 }
